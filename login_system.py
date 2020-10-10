54bbbd063ae5d763f21c46b7308ccff93515a0f3
SeanPhilippi <sean.philippi@gmail.com>
2020-10-10 14:55:25 -0500

    prompts and breaks in place, not saving though

--- login_system.py
+++ login_system.py
@@ -1,22 +1,36 @@
+from database import users
+
+def usernames(users):
+    usernames = []
+    for user in users:
+        [username, password] = user
+        usernames.append(username)
+    return usernames
+
+
 account = input("Would you like to create an account? Answer 'yes' or 'no'.\n")
 if account == 'yes' or account == 'y':
     while True:
         username = input("Enter a username: ")
-        if username in usernames:
+        
+        if username in usernames(users):
             print('Username has already been taken')
         else:
             while True:
-                password = input("Enter a password")
+                password = input("Enter a password: ")
                 # check password for length, special characters, and capitalized character
                 # in future, maybe give a rating based on password strength, find a library for this?
-                if length(password) < 8:
+                if len(password) < 8:
                     print("Password is too short. Must be at least 8 characters")
                 else:
-                    confirm_password = input("Enter your password again to confirm")
+                    confirm_password = input("Enter your password again to confirm: ")
                     if password == confirm_password:
+                        users.append({ username: username, password: password })
                         print("Account successfully created!")
+                        print("users:", users)
                         break
                     else:
                         print("Passwords do not match.")
+        break
 elif account == 'no' or 'n':
     exit()
